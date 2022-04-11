b1c201f69651ced12aed477ef40e0421ba841cdb
memokal <memokall@gmail.com>
2022-04-11 03:32:36 +0300

    Css Fixes and Javascript almost Complete
    I added width:ch and overflow on .screen-bottom to fix issues about overflowing.
    
    Javascript:
    added functions :
    numbers() :+1:
    calculations() :+1:
    calculate() :+1:
    
    summary:
    - numbers can be added innerHTML of #screen-bottom also can be removed :+1:
    - calculation symbols are being controlled for not having duplications also some bugs with it are solved simple ways. :+1:
    - string calculation added :airplane:

---
+++ Js/Javascript.js
@@ -0,0 +1,75 @@
+var noDuplicate = false;
+
+function number(x){ //gets element
+    var num=x.value;  //gets element's value and equals it to variable num   
+    document.getElementById("screen-bottom").innerHTML=document.getElementById("screen-bottom").innerHTML+num; //writes the numer into innerHTML and memorizes it
+    noDuplicate=false; //it means we can place a calculation symbol after a number
+}
+
+function calculations(x){
+    var a=x.value;
+    switch(a){
+        case "+" : //if value is a "+"
+            var calc=document.getElementById("screen-bottom").innerHTML;
+            if(noDuplicate==false){ //checking duplication of calculation symbols
+                calc=calc+"+"; //places a "+" on memory
+                document.getElementById("screen-bottom").innerHTML=calc; // writes memory to innerHTML
+                noDuplicate=true; // marks that we placed a "+" in the end of string
+            }else{ // if theres a duplication of calculation symbols
+                calc = calc.substring(0, calc.length - 1); //removes last letter or symbol
+                document.getElementById("screen-bottom").innerHTML=calc+"+"; //replaces with a new symbol, so we can dynamically change between symbols
+            }
+        break;
+        case "-" :
+            var calc=document.getElementById("screen-bottom").innerHTML;
+            if(noDuplicate==false){
+                calc=calc+"-";
+                document.getElementById("screen-bottom").innerHTML=calc;
+                noDuplicate=true;
+            }else{
+                calc = calc.substring(0, calc.length - 1);
+                document.getElementById("screen-bottom").innerHTML=calc+"-";
+            }
+        break;
+        case "×" :
+            var calc=document.getElementById("screen-bottom").innerHTML;
+            if(noDuplicate==false){
+                calc=calc+"*";
+                document.getElementById("screen-bottom").innerHTML=calc;
+                noDuplicate=true;
+            }else{
+                calc = calc.substring(0, calc.length - 1);
+                document.getElementById("screen-bottom").innerHTML=calc+"*";
+            }
+        break;
+        case "÷" :
+            var calc=document.getElementById("screen-bottom").innerHTML;
+            if(noDuplicate==false){
+                calc=calc+"/";
+                document.getElementById("screen-bottom").innerHTML=calc;
+                noDuplicate=true;
+            }else{
+                calc = calc.substring(0, calc.length - 1);
+                document.getElementById("screen-bottom").innerHTML=calc+"/";
+            }
+        break;
+        case "AC" :
+            //picks innerHTML and removes last string - v below v
+            var calc=document.getElementById("screen-bottom").innerHTML;
+            calc = calc.substring(0, calc.length - 1);
+            document.getElementById("screen-bottom").innerHTML=calc;
+            //checks if the last word is a calculation - v below v 
+            var control=calc.charAt(calc.length-1);
+            if(control=="+" || control=="-" || control=="/" || control=="*"){
+                noDuplicate=true;
+            }else{noDuplicate=false;}  
+        break;
+    }
+}
+
+function calculate(){
+    var calc=document.getElementById("screen-bottom").innerHTML;
+    document.getElementById("screen-top").innerHTML=calc+"=";
+    var result = eval(calc); //im aware of eval() but im a beginner javascripter so i cannot build my own string calc function i will see it later.
+    document.getElementById("screen-bottom").innerHTML=result;
+}
